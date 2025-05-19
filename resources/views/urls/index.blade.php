83c36c7a7d701bf0a4e737543fdcc4ebc4104e69
Mashima Takeo <urkn99343@me.pikara.ne.jp>
2025-05-19 16:09:12 +0900

    1：38まで

--- resources/views/urls/index.blade.php
+++ resources/views/urls/index.blade.php
@@ -8,7 +8,36 @@
     <!-- tailwindcss cdn -->
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body class="bg-gray-100">
+<body class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-10/12">
+        <form action="{{ route('urls.store') }}" id="url-form" method="post">
+            @csrf
+            <input type="text" name="original_url" id="original_url" class="border border-gray-300 p-2 my-2 w-full" placeholder="Enter URL" required>
+            <button type="su7bmit" class="bg-blue-500 text-white p-2 mt-4 rounded w-full">短縮する</button>
+        </form>
+        <div id="result" class="mt-4"></div>
+    </div>
 
+    <script>
+        const urlForm = document.querySelector('#url-form')
+        const originalUrl = document.querySelector('#original_url').value;
+        console.log(originalUrl);
+        urlForm.addEventListener('submit', async(event) => {
+            event.preventDefault();
+
+            const response = await fetch('/urls', {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json',
+                    'X-CSRF-TOKEN': '{{csrf_token()}}',
+                },
+                body: JSON.stringify({ original_url : originalUrl}),
+            });
+            
+            const data = await response.json();
+            document.querySelector('#result').innerHTML = '<a href="${data.shortend_url}" target="_blank">${data.shortend_url}</a>'
+            
+        })
+    </script>
 </body>
 </html>
